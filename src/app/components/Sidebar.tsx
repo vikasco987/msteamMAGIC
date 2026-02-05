fe7097c5c1e65fdb39f0304541230735be898d17
vikasco987 <vikasvikas9095@gmail.com>
2026-02-05 18:25:59 +0530

    add

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1594,7 +1594,7 @@ const allNavItems = [
   { label: 'Assigned Task', icon: ClipboardCheck, href: '/report', roles: ['admin', 'master', 'seller'] },
   { label: 'KAM', icon: Building2, href: '/kam', roles: ['admin', 'master', 'seller'] },
   { label: 'Timeline', icon: LineChart, href: '/timeline', roles: ['admin', 'master', 'seller', 'temp'] },
-  { label: 'MySales Dashboard', icon: ShoppingCart, href: '/seller/dashboard', roles: ['seller', 'admin'] },
+  { label: 'MySales Dashboard', icon: ShoppingCart, href: '/seller/dashboard', roles: ['seller', 'admin','master'] },
   { label: 'Sales Dashboard', icon: BarChart3, href: '/sales-dashboard', roles: ['master'] },
 
   // Agreement
